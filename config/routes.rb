aaf7663cb51d2c3e09aaf501770c6f72cd1485b5
halogenandtoast <halogenandtoast@gmail.com>
2014-03-10 17:42:52 -0400

    Add, Edit, Update, Destroy galleries

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Pixtr::Application.routes.draw do
   root "galleries#index"
+  get "/galleries/new" => "galleries#new"
+  get "/galleries/:id" => "galleries#show",
+    as: :gallery
+  post "/galleries" => "galleries#create"
+  get "/galleries/:id/edit" => "galleries#edit"
+  patch "/galleries/:id" => "galleries#update"
+  delete "/galleries/:id" => "galleries#destroy"
 end
